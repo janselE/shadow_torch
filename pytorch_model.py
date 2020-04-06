0a3586a98ff246c37e1ea3545f5e2915dbe857de
janselE <jansel.herrera@gmail.com>
2020-04-05 22:50:38 -0500

    .

--- pytorch_model.py
+++ pytorch_model.py
@@ -165,10 +165,13 @@ def train_model(epochs):
 
 train_model(10)
 
+print("Starting prediction")
 with torch.no_grad():
     predicted = model(t_imgs.float()).data.numpy()
     org = t_imgs.numpy()
     res = t_target.numpy()
+
+print("Done predicting, staring to write on files")
 
 predicted = predicted.reshape(-1, 480, 640)
 org = org.reshape(-1, 480, 640)
@@ -177,6 +180,8 @@ res = res.reshape(-1, 480, 640)
 for i in range(0, len(predicted)):
     name = 'predicted/img' + str(i) + '.png'
     cv2.imwrite(name, predicted[i])
+
+print("Done writing")
 
 
 
