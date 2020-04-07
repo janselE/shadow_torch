3e52f42da307e9c03347889b036395b0592216f2
janselE <jansel.herrera@gmail.com>
2020-04-06 20:25:43 -0500

    .

--- pytorch_model.py
+++ pytorch_model.py
@@ -174,6 +174,7 @@ class UNet(nn.Module):
 
         print("Transpose layer")
         out = self.layer4(out)
+        print(out.shape)
         out = torch.cat((out, out1), 0)
         print("Concat layer")
         print(out.shape)
