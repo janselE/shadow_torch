2baf7f8fd94b163461c782640fc3c777aa5be25c
janselE <jansel.herrera@gmail.com>
2020-05-28 00:56:55 -0500

    Changing dimensions

--- train_category_removal.py
+++ train_category_removal.py
@@ -300,7 +300,7 @@ for epoch in range(0, num_epochs):
                 # this consecutive lines are for the image on tensorboard
                 if curr % 500 == 0:
                     img_to_board = torch.argmax(x1_outs[0][0].cpu().detach(), dim=1).numpy()  # gets black and white image
-                    o = img1[0][0].cpu().detach()
+                    o = img1[0].cpu().detach()
                     writer.add_image('val_original', o, curr)
                     writer.add_image('val_images', img_to_board, curr)
 
@@ -320,7 +320,7 @@ for epoch in range(0, num_epochs):
                 # this consecutive lines are for the image on tensorboard
                 if curr % 500 == 0:
                     img_to_board = torch.argmax(x1_outs[0][0].cpu().detach(), dim=1).numpy()  # gets black and white image
-                    o = img1[0][0].cpu().detach()
+                    o = img1[0].cpu().detach()
                     writer.add_image('train_original', o, curr)
                     writer.add_image('train_images', img_to_board, curr)
 
