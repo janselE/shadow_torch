ca90c75ce2baf599579831b3ee2602f4680bc57a
janselE <jansel.herrera@gmail.com>
2020-05-28 01:10:53 -0500

    merge

--- train_category_removal.py
+++ train_category_removal.py
@@ -320,7 +320,7 @@ for epoch in range(0, num_epochs):
                 # this consecutive lines are for the image on tensorboard
                 if curr % 500 == 0:
                     img_to_board = torch.argmax(x1_outs[0].cpu().detach(), dim=1).numpy()  # gets black and white image
-                    print(img_to_board[0].shape)
+                    print(img_to_board.shape)
                     print(img1[0].shape)
                     exit()
                     o = img1[0].cpu().detach()
