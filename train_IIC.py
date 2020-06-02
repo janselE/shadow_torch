cfdc4033c3f7ccbc6740865247f5f7266599f991
janselE <jansel.herrera@gmail.com>
2020-06-02 00:10:21 -0500

    debbug

--- train_IIC.py
+++ train_IIC.py
@@ -175,8 +175,6 @@ for epoch in range(0, num_epochs):
         if curr % 500 == 0:
             o = img1[0].cpu().detach()
             img_to_board = torch.argmax(x1_outs[0].cpu().detach(), dim=1).numpy()[0]  # gets black and white image
-            print(x1_outs[0].shape)
-            print(img_to_board.shape)
             color = color_mapper.add_color(img_to_board) # this is where we send the mask to the scrip
 
             # here we add the images to the writer
