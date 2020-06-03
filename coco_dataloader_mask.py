199c3e88a9868e0f03f089e11f56e5997eea7651
janselE <jansel.herrera@gmail.com>
2020-06-03 00:00:27 -0500

    trying to find the error

--- coco_dataloader_mask.py
+++ coco_dataloader_mask.py
@@ -167,7 +167,8 @@ class CocoDataloader(torch.utils.data.Dataset):
 
         new_labels = np.zeros(labels.shape, dtype=labels.dtype)
         for c in range(0, 182):
-            new_labels[labels == c] = self._fine_to_coarse_dict[c]
+           print(self._fine_to_coarse_dict[c])
+        #    new_labels[labels == c] = self._fine_to_coarse_dict[c]
         labels = new_labels
 
         print(type(img))
